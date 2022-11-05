5eafe19e0fb877ade750e001930a6dc568866aec
EC2 Default User <ymy0190@gmail.com>
2022-11-05 03:02:42 +0000

    postcommentのurl直打ち対策

--- app/controllers/customer/post_comments_controller.rb
+++ app/controllers/customer/post_comments_controller.rb
@@ -1,12 +1,17 @@
 class Customer::PostCommentsController < ApplicationController
-  before_action :ensure_user, only: [:edit, :update, :destroy]
-  
+  before_action :set_postcomment, only: [:edit, :update, :destroy]
+  before_action :prevent_url, only: [:edit, :update, :destroy]
+
   def create
     @post = Post.find(params[:post_id])
     comment = @post.post_comments.new(post_comment_params)
     comment.customer_id = current_customer.id
     comment.save
     redirect_to customer_post_path(@post)
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
   end
 
   def destroy
@@ -19,10 +24,14 @@ class Customer::PostCommentsController < ApplicationController
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
-  def ensure_user
-    @posts = current_customer.posts
-    @post = @posts.find_by(id: params[:id])
-    redirect_to new_customer_post_path unless @post
+
+  def set_postcomment
+      @post = Post.find(params[:id])
+  end
+
+  def prevent_url
+    if @post.customer_id != current_customer.id
+      redirect_to root_path
+    end
   end
 end
